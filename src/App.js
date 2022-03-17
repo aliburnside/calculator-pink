d9632e2d699ee55672017887155ee2e5a919818f
Alexandra Burnside <burnsideally@gmail.com>
2022-03-17 11:48:31 +0000

    architecture - html and css ready

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="calculator-box">
+          <h1>Calculator</h1>
+          <div className="button-area">
+            <div className="first-top-num-row">
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+              <button>AC</button>
+            </div>
+            <div className="second-top-num-row">
+              <button>4</button>
+              <button>5</button>
+              <button>6</button>
+              <button>/</button>
+            </div>
+            <div className="third-top-num-row">
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>x</button>
+            </div>
+            <div className="fourth-top-num-row">
+              <button>0</button>
+              <button>-</button>
+              <button>+</button>
+              <button>=</button>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
